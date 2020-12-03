d952e23c44abd4aae3a7277138837fad84c4f2e9
TrifonovD <dmitritrifonov22@gmail.com>
2020-12-03 15:57:08 +0300

    initialize project

---
+++ App/Root.jsx
@@ -0,0 +1,68 @@
+import React, {useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import Orientation from 'react-native-orientation';
+
+const Root = () => {
+  useEffect(() => {
+    Orientation.lockToLandscape();
+  }, []);
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle="dark-content" />
+      <Text>12345</Text>
+    </SafeAreaView>
+  );
+};
+
+const s = StyleSheet.create({
+  scrollView: {
+
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default Root;
+
